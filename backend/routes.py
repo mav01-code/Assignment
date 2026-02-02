7ae927fa58d5e2cc9a4710ebcf517cc10326e18e
mav01-code <marreddyakshayavarshini@gmail.com>
2026-02-03 00:40:39 +0530

    Server logic is completed

---
+++ backend/routes.py
@@ -0,0 +1,5 @@
+def getQuestions():
+    pass
+
+def getGrade():
+    pass
---
+++ backend/service.py
@@ -0,0 +1,9 @@
+def get_questions():
+    pass
+
+def calculate_grade(score):
+    total = (score//400)*100
+    return total
+
+def get_grade(score):
+    return calculate_grade(score)
