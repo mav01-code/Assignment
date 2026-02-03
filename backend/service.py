c1786100e3ae1b972cf5205a7bb678a3d7663962
mav01-code <marreddyakshayavarshini@gmail.com>
2026-02-04 03:50:45 +0530

    Server logic

--- backend/service.py
+++ backend/service.py
@@ -1,5 +1,37 @@
-def get_questions():
-    pass
+import json
+import pyttsx3
+import whisper
+import os
+import ffmpeg
+
+def get_questions(role):
+    file = "roles.json"
+    with open(file, "r") as f:
+        data = json.load(f)
+    if role in data:
+        return data[role]["questions"]
+    else:
+        return "Not found"
+
+engine = pyttsx3.init()
+
+def speak_question(question):
+    print(question)
+    engine.say(question)
+    engine.runAndWait()
+
+model = whisper.load_model("base")
+
+def stt(audiofile):
+    os.makedirs("audiofiles", exist_ok = True)
+    os.makedirs("transcript", exist_ok=True)
+    (ffmpeg.input(audiofile).output(audiofile, ac = 1, ar = 16000).overwrite_output().run(quiet = True))
+    res = model.transcribe(audiofile)
+    transcript = res["text"]
+    return transcript
+
+def get_answer(audio):
+    return stt(audio)
 
 def calculate_grade(score):
     total = (score//400)*100
