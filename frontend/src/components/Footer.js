198d26a90d1c6ad528b105821e7472315ba77fdb
mav01-code <marreddyakshayavarshini@gmail.com>
2026-02-04 18:49:25 +0530

    Completed Header

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/Footer.css"
+
+function Footer(){
+    return(
+        <footer></footer>
+    );
+}
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-logo">Robyyn</div>
+
+      <button className="header-cta">
+        Get Started
+      </button>
+    </header>
+  );
+}
+
+export default Header;
